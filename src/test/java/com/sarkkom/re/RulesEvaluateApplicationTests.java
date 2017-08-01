348b3fb0d0408e31a20d1a7060449ffaf0305e75
Ajay Koranne <akoranne@gmail.com>
2017-08-01 10:20:02 -0500

    changes to use spring boot 1.5.6

--- src/test/java/com/sarkkom/re/RulesEvaluateApplicationTests.java
+++ src/test/java/com/sarkkom/re/RulesEvaluateApplicationTests.java
@@ -7,18 +7,15 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Map;
 
 import static junit.framework.TestCase.fail;
 
-//import org.springframework.boot.test.context.SpringBootTest;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-// @SpringBootTest(classes = RulesEvaluateApplication.class)
-@SpringApplicationConfiguration(classes = RulesEvaluateApplication.class)
+@SpringBootTest(classes = RulesEvaluateApplication.class)
 public class RulesEvaluateApplicationTests {
 
 	private static final Logger log = LoggerFactory.getLogger(RulesEvaluateApplicationTests.class);
--- src/test/java/com/sarkkom/re/WhatToDoRuleTest.java
+++ src/test/java/com/sarkkom/re/WhatToDoRuleTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.script.ScriptEngine;
@@ -20,7 +20,7 @@ import static junit.framework.TestCase.fail;
  * Created by ajaykoranne on 4/21/16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RulesEvaluateApplication.class)
+@SpringBootTest(classes = RulesEvaluateApplication.class)
 public class WhatToDoRuleTest {
 	private static final Logger log = LoggerFactory.getLogger(WhatToDoRuleTest.class);
 
