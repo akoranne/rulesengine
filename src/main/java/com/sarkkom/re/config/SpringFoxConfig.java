8b6b1fce888528793bb7e9d2db3761b058859247
Ajay Koranne <akoranne@gmail.com>
2018-05-10 21:53:23 -0500

    updated to 1.5.13.RELEASE, added swagger

---
+++ src/main/java/com/sarkkom/re/config/SpringFoxConfig.java
@@ -0,0 +1,37 @@
+package com.sarkkom.re.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile({"default", "cloud"})
+public class SpringFoxConfig {
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.sarkkom"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("simple-rules")
+				.description("Simple Rules Engine")
+				.termsOfServiceUrl("http://localhost:8080/")
+				.contact(new Contact("Ajay S. Koranne", "", "akoranne"))
+				.version("1.0")
+				.build();
+	}
+}
